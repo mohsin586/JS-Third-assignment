9bb458c238329ca1b9c4c332b1fed75926f714f1
mohsin586 <143820469+mohsin586@users.noreply.github.com>
2023-09-15 15:21:35 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,227 @@
+//student.splice(0,2)      delete arry elements.
+//student.splice(1,0)        add elements.
+
+//student.slice(2,5);  copy 
+
+// var info = ["ali", 13, "gb"];
+// info.pop();   delete from last
+// info.push();   add from  last
+// info.shift();
+// info.shift();
+//var convrt = info.join();    make a single string 
+
+
+// console.log(info);
+
+// var ary = [];
+// ary[0] = prompt();
+// ary[1] = +prompt();
+
+// if (ary[1] == 13) {
+
+//     console.log("come here")
+
+// }
+
+// console.log(ary);
+
+
+
+//07
+// var adu = ["SSC", "HSC", "BCS", "BS", "Bcom", "Ms", "Mphil", "phd"];
+
+// document.writeln("Qualifications:<br>" + adu)
+
+
+//console.log("all is well");
+// var stu = ["ali", "aslam", "khan"];
+// var scr = [320, 230, 480];
+// console.log("score of ali is  " + scr[0] + "  percentage: " + (scr[0] / 500) * 100 + "%");
+// console.log("score of Aslam is  " + scr[1] + "  percentage: " + (scr[1] / 500) * 100 + "%");
+// console.log("score of Khan is  " + scr[2] + "  percentage: " + (scr[2] / 500) * 100 + "%");
+
+
+// 09
+// document.write('Original Array:', colorArray);
+
+// // a. Add a color to the beginning of the array
+// const colorToAddAtBeginning = prompt('Enter a color to add to the beginning:');
+// colorArray.unshift(colorToAddAtBeginning);
+
+// // Display the updated array
+// document.write('Array after adding color to the beginning:', colorArray);
+
+// // b. Add a color to the end of the array
+// const colorToAddAtEnd = prompt('Enter a color to add to the end:');
+// colorArray.push(colorToAddAtEnd);
+
+// // Display the updated array
+// document.write('Array after adding color to the end:', colorArray);
+
+// // c. Add two more colors to the beginning of the array
+// colorArray.unshift('Purple', 'Pink');
+
+// // Display the updated array
+// document.write('Array after adding two more colors to the beginning:', colorArray);
+
+// // d. Delete the first color in the array
+// colorArray.shift();
+
+// // Display the updated array
+// document.write('Array after deleting the first color:', colorArray);
+
+// // e. Delete the last color in the array
+// colorArray.pop();
+
+// // Display the updated array
+// document.write('Array after deleting the last color:', colorArray);
+
+// // f. Add a color at a specific index
+// const indexToAddColor = prompt('Enter the index to add a color:');
+// const colorToAddAtIndex = prompt('Enter the color to add at the specified index:');
+// colorArray.splice(indexToAddColor, 0, colorToAddAtIndex);
+
+// // Display the updated array
+// document.write('Array after adding a color at the specified index:', colorArray);
+
+// // g. Delete color(s) from a specific index
+// const indexToDelete = prompt('Enter the index from which to delete color(s):');
+// const numberOfColorsToDelete = prompt('Enter the number of colors to delete:');
+// colorArray.splice(indexToDelete, numberOfColorsToDelete);
+
+// // Display the updated array
+// document.write('Array after deleting color(s) from the specified index:', colorArray);
+
+
+// 09
+// document.write('Original Array:', colorArray);
+
+// // a. Add a color to the beginning of the array
+// const colorToAddAtBeginning = prompt('Enter a color to add to the beginning:');
+// colorArray.unshift(colorToAddAtBeginning);
+
+// // Display the updated array
+// document.write('Array after adding color to the beginning:', colorArray);
+
+// // b. Add a color to the end of the array
+// const colorToAddAtEnd = prompt('Enter a color to add to the end:');
+// colorArray.push(colorToAddAtEnd);
+
+// // Display the updated array
+// document.write('Array after adding color to the end:', colorArray);
+
+// // c. Add two more colors to the beginning of the array
+// colorArray.unshift('Purple', 'Pink');
+
+// // Display the updated array
+// document.write('Array after adding two more colors to the beginning:', colorArray);
+
+// // d. Delete the first color in the array
+// colorArray.shift();
+
+// // Display the updated array
+// document.write('Array after deleting the first color:', colorArray);
+
+// // e. Delete the last color in the array
+// colorArray.pop();
+
+// // Display the updated array
+// document.write('Array after deleting the last color:', colorArray);
+
+// // f. Add a color at a specific index
+// const indexToAddColor = prompt('Enter the index to add a color:');
+// const colorToAddAtIndex = prompt('Enter the color to add at the specified index:');
+// colorArray.splice(indexToAddColor, 0, colorToAddAtIndex);
+
+// // Display the updated array
+// document.write('Array after adding a color at the specified index:', colorArray);
+
+// // g. Delete color(s) from a specific index
+// const indexToDelete = prompt('Enter the index from which to delete color(s):');
+// const numberOfColorsToDelete = prompt('Enter the number of colors to delete:');
+// colorArray.splice(indexToDelete, numberOfColorsToDelete);
+
+// // Display the updated array
+// document.write('Array after deleting color(s) from the specified index:', colorArray);
+
+
+
+//10
+
+// var stu = [320, 230, 480, 120];
+// var sort = stu.sort();
+// console.log(sort);
+
+//11
+
+// var city = ["khi", "isl", "lahore", "skd", "GB"];
+
+// var copy = city.slice(2, 6);
+
+// console.log(copy);
+
+//12
+// var arr = ["this", "is", "my", "cat"];
+// console.log(typeof arr);
+// console.log(arr);
+
+
+// var jo = arr.join();
+
+// console.log(typeof jo);
+// console.log(jo);
+
+//13
+// var cs = ["keyboard", "mouse", "printer", "monitor"];
+// console.log(cs)
+
+
+// cs.shift();
+// console.log("out: \n keyboard");
+
+// console.log(cs);
+// cs.shift();
+// console.log("out: \n mouse");
+
+// console.log(cs);
+
+// cs.shift();
+// console.log("out: \n printer");
+
+// console.log(cs);
+
+// cs.shift();
+// console.log("out: \n monitor");
+
+// console.log(cs);
+
+//14
+// var cs = ["keyboard", "mouse", "printer", "monitor"];
+// console.log(cs)
+
+
+// cs.pop();
+// console.log("out:  monitor");
+
+// console.log(cs);
+// cs.pop();
+// console.log("out: printer");
+
+// console.log(cs);
+
+// cs.pop();
+// console.log("out:  mouse");
+
+// console.log(cs);
+
+// cs.pop();
+// console.log("out:  keyboard");
+
+// console.log(cs);
+
+//15
+// var cell = ["Apple", "Samsung", "Motrola", "Nokia", "Sony", "Vivo"];
+// console.log(cell[2])
+
+// document.writeln("ssss <br" + cell)
+
